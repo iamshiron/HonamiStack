cddb4ede154588c6f14adee6727d49f63f6d6a88
Shiron <contact@shiron.io>
2026-03-19 10:53:15 +0100

    feat: Added basic config types

--- src/CLI/Commands/AsyncInitCommand.cs
+++ src/CLI/Commands/AsyncInitCommand.cs
@@ -1,16 +1,33 @@
-﻿using Shiron.HonamiStack.Core.Services;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+using Shiron.HonamiStack.Core;
+using Shiron.HonamiStack.Core.Services;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Shiron.HonamiStack.CLI.Commands;
 
+[UsedImplicitly]
 public class AsyncInitCommand(ILogger logger) : AsyncCommand<AsyncInitCommand.Settings> {
     private readonly ILogger _logger = logger;
+    private readonly JsonSerializerOptions _jsonOptions = new() {
+        IndentSize = 4,
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
 
+    [UsedImplicitly]
     public sealed class Settings : CommandSettings {
     }
 
-    public override Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken) {
-        _logger.Log($"Current Working Directory: {Directory.GetCurrentDirectory()}");
-        return Task.FromResult(CliConstants.ExitCodes.Success);
+    public async override Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken) {
+        var config = new Config("Test", [
+            new FrontendModule("web", "apps/web"),
+            new BackendModule("core", "backend/core")
+        ]);
+
+        await File.WriteAllTextAsync("honami.config.json", JsonSerializer.Serialize(config, _jsonOptions), cancellationToken);
+        return 0;
     }
 }
---
+++ src/Core/Config.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shiron.HonamiStack.Core;
+
+public record Config(string Name, List<Module> Modules);
+
+[JsonDerivedType(typeof(BackendModule))]
+[JsonDerivedType(typeof(FrontendModule))]
+public abstract class Module(string name, string location) {
+    public string Name { get; } = name;
+    public string Location { get; } = location;
+}
+
+public class BackendModule(string name, string location) : Module(name, location) {
+}
+
+public class FrontendModule(string name, string location) : Module(name, location) {
+}
